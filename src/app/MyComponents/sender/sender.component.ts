3332d13e10f7b344e404e725eaf65e65308e6fea
Ruttab Haroon <ruttab.haroon@eplanetcom.com>
2021-09-27 19:33:29 +0500

    fixedd

--- src/app/MyComponents/sender/sender.component.ts
+++ src/app/MyComponents/sender/sender.component.ts
@@ -12,6 +12,11 @@ import { SendSMSResponse } from 'src/app/Classes/SMS/send_sms_response';
 })
 export class SenderComponent implements OnInit {
 
+  source: string = "node.js";
+  body: string = "Hello there";
+  to: string = "+61411111111,+61422222222";
+  custom_string: string = "this is a test";
+
   response: SendSMSResponse | undefined;
 
 
@@ -21,16 +26,23 @@ export class SenderComponent implements OnInit {
   }
 
   actionSendSMS() {
-    const m : MyMessage = {
-      source : "php",
-      body : "Hello there",
-      to : "{+61411111111}",
-      custom_string : "this is a test",
-      schedule: 1632731133,
-    };
-    const param : SendSMSParam = {messages: [m]};
-    this.apiService.sendSMS(param)
-    .subscribe(response => {this.response = response});
+    var messagesList : MyMessage[] = [] ;
+    var splitted = this.to.split(","); 
+    if (splitted.length > 0) {
+        splitted.forEach((element) => { 
+          const m : MyMessage = {
+            source : this.source,
+            body : this.body,
+            to : element,
+            custom_string : this.custom_string
+            //schedule: 1632731133,
+          };
+          messagesList.push(m)
+        });
+        console.log(messagesList)
+        const param : SendSMSParam = {messages: messagesList};
+        this.apiService.sendSMS(param)
+          .subscribe(response => {this.response = response});
+    }
   }
-
 }
