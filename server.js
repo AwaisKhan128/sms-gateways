0c99e81a0ef8d47acbe45454a78b23b8d26f0b2d
Awais <awais.khan128@yahoo.com>
2021-10-11 16:55:32 +0500

    new

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('dist/SMS-GATEWAY'));
+app.use(express.static('./dist/SMS-Gateway'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('index.html',{ root: './dist/SMS-GATEWAY/' }));
+res.sendFile('index.html',{ root: 'dist/SMS-Gateway/' });
 });
 
 // Start the app by listening on the default Heroku port
