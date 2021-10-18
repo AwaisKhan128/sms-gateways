383768ac814df8cb404ad220bdbee10ddf06290e
Awais <awais.khan128@yahoo.com>
2021-10-19 03:24:55 +0500

    aaa

--- src/app/operator-configuration/operator-configuration.component.ts
+++ src/app/operator-configuration/operator-configuration.component.ts
@@ -12,4 +12,48 @@ export class OperatorConfigurationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  Operator(){
+    $("#operator").addClass('active');
+    $("#add_sms").removeClass('active');
+    $("#add_ussd").removeClass('active');
+    $("#send_sms").removeClass('active');
+    $("#send_ussd").removeClass('active');
+
+  }
+  Add_SMS()
+  {
+    $("#add_sms").addClass('active');
+    $("#operator").removeClass('active');
+    $("#add_ussd").removeClass('active');
+    $("#send_sms").removeClass('active');
+    $("#send_ussd").removeClass('active');
+  }
+  Add_USSD()
+  {
+    $("#add_ussd").addClass('active');
+    $("#add_sms").removeClass('active');
+    $("#operator").removeClass('active');
+    $("#send_sms").removeClass('active');
+    $("#send_ussd").removeClass('active');
+  }
+  Send_SMS()
+  {
+    $("#send_sms").addClass('active');
+    $("#add_sms").removeClass('active');
+    $("#add_ussd").removeClass('active');
+    $("#operator").removeClass('active');
+    $("#send_ussd").removeClass('active');
+  }
+
+  Send_USSD()
+  {
+    $("#send_ussd").addClass('active');
+    $("#add_sms").removeClass('active');
+    $("#add_ussd").removeClass('active');
+    $("#send_sms").removeClass('active');
+    $("#operator").removeClass('active');
+
+  }
+
 }
