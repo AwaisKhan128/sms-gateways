ed42c9b522d6bd3e12d6a7ba2e25405c24699e8a
Ruttab Haroon <rottabhq@hotmail.com>
2021-10-21 11:11:03 +0500

    fixeddd

--- src/app/MyComponents/Childs/Operators/send-sms-inquiry/send-sms-inquiry.component.ts
+++ src/app/MyComponents/Childs/Operators/send-sms-inquiry/send-sms-inquiry.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SendSMSInquiryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: API_Services) { }
 
   ngOnInit(): void {
+  }
+
+  getOperators() {
+    this.apiService.getlistofOperators().subscribe(
+      e=>{
+        const ops = e.http_response as PhoneOperator[]
+        let k : PhoneOperator = {
+          id: -1,
+          operator_name: "NONE",
+          operator_code: "NONE"
+        }
+        ops.push(k)
+        console.log("ops 0",ops[0])
+        this.operators = ops
+      }
+    )
+  }
+
+  getListOfDevicesForOperator(opcode: string) {
+      var i = 0
+      this.apiService.getlistofDevicesForOperator(opcode).subscribe(
+        e=> {
+          this.phoneNumbers = [];
+          const d = e.http_response as DevicesMatchingOperator[]
+          console.log(d)
+          d.forEach(e=>{
+            i+= 1
+            e.number! += i
+            e.isDisabled = true
+            e.defaultUSSDReply = "N/A"
+            e.defaultUSSDStatus = "Not Send"
+            e.ussdCodeToSend = ""
+          })
+          this.phoneNumbers = d
+        }
+      )
   }
 
 }
